965506b5c9a29b07085582bddc6141dd37a761eb
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-13 14:11:23 +0300

    feature (game.js) distributing cards

--- src/game.js
+++ src/game.js
@@ -24,5 +24,40 @@ export default (nameOfPlayer1, testFunc) => {
     return iter(names, ranges, suits, pack);
   };
   const pack = testFunc ? testFunc(consOfPack()) :
-  consOfPack().sort(() => Math.random() > 0.5 ? 1 : -1);
+  consOfPack().sort(() => Math.random() > 0.5 ? 1 : -1);/* potential Error;
+  if testFunc be entered by user may be it'll require try...catch block*/
+  // making players
+  const player1 = new Player(nameOfPlayer1, []);
+  const player2 = new Player('Player2', []);
+  // distribute cards between of players
+  const startDistribute = (gambler1, gambler2, cards) => {
+    if (gambler1.cards.length === 6 && gambler2.cards.length === 6) {
+      return [gambler1, gambler2];
+    }
+    let topCard;
+    topCard = cards.pop();
+    gambler1.take(topCard);
+    topCard = cards.pop();
+    gambler2.take(topCard);
+    return startDistribute(gambler1, gambler2, cards);
+  };
+  const playersWithCards = startDistribute(player1, player2, pack);
+  // definition of trump
+  const defineTrump = () => {
+    const defTrump = pack.pop();
+    const trumpSuit = defTrump.suit;
+    playersWithCards.map(player => player.cards.map((card) => {
+      const trump = card.suit === trumpSuit ? card.type = 'trump' : card;
+      return trump;
+    }));
+    const topOfPack = [defTrump];
+    // newPack.
+    topOfPack.push(...pack);
+    const newPack = topOfPack.map((card) => {
+      card.suit === trumpSuit ? card.type = 'trump' : card;
+      return card;
+    });
+    return newPack;
+  };
+  return defineTrump();
 };
