df71e95157cca13ac0da888554baca949ca01c67
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-14 18:28:09 +0300

    fix and refactor (game.js) function defineWhoStart

--- src/game.js
+++ src/game.js
@@ -6,22 +6,22 @@ export default (nameOfPlayer1, testFunc) => {
   const consOfPack = () => {
     const pack = [];
     const names = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6'];
-    const ranges = [14, 13, 12, 11, 10, 9, 8, 7, 6];
+    const seniority = [14, 13, 12, 11, 10, 9, 8, 7, 6];
     const suits = ['heart', 'club', 'spade', 'diamond'];
-    const iter = (ArrOfNames, ArrOfRanges, ArrOfSuits, acc) => {
+    const iter = (ArrOfNames, ArrOfSeniority, ArrOfSuits, acc) => {
       if (ArrOfNames.length === 0) {
         return acc;
       }
       const itemOfName = ArrOfNames.pop();
-      const itemOfRange = ArrOfRanges.pop();
-      const heart = new Card(itemOfName, itemOfRange, ArrOfSuits[0], 'ordinary');
-      const diamond = new Card(itemOfName, itemOfRange, ArrOfSuits[1], 'ordinary');
-      const spade = new Card(itemOfName, itemOfRange, ArrOfSuits[2], 'ordinary');
-      const club = new Card(itemOfName, itemOfRange, ArrOfSuits[3], 'ordinary');
+      const itemOfSeniority = ArrOfSeniority.pop();
+      const heart = new Card(itemOfName, itemOfSeniority, ArrOfSuits[0], 'ordinary');
+      const diamond = new Card(itemOfName, itemOfSeniority, ArrOfSuits[1], 'ordinary');
+      const spade = new Card(itemOfName, itemOfSeniority, ArrOfSuits[2], 'ordinary');
+      const club = new Card(itemOfName, itemOfSeniority, ArrOfSuits[3], 'ordinary');
       acc.push(heart, diamond, spade, club);
-      return iter(ArrOfNames, ArrOfRanges, ArrOfSuits, acc);
+      return iter(ArrOfNames, ArrOfSeniority, ArrOfSuits, acc);
     };
-    return iter(names, ranges, suits, pack);
+    return iter(names, seniority, suits, pack);
   };
   const pack = testFunc ? testFunc(consOfPack()) :
   consOfPack().sort(() => Math.random() > 0.5 ? 1 : -1);/* potential Error;
@@ -60,34 +60,51 @@ export default (nameOfPlayer1, testFunc) => {
   };
   const newPack = makeTrumps();
   const defineWhoStart = (players) => {
-    const playersWithTrumps = players.map(player => player.cards.filter(card => card.type === 'trump'));
-    const pl1 = playersWithTrumps[0];
-    const pl2 = playersWithTrumps[1];
-    const compareCards = ([player1, player2]) => {
-      const cardOfPlayer1 = player1.cards.pop(); // the least card of player 1
-      const cardOfPlayer2 = player2.cards.pop(); // the least card of player 2
-      // if (player1.cards === undefined || ){
-        if (cardOfPlayer1.range !== cardOfPlayer2.range) {
-          return cardOfPlayer1.range < cardOfPlayer2.range ? player1 : player2;
-        }
-      }
-      return compareCards([player1, player2]);
-    };
-    if (pl1.cards.length === 0 && pl2.cards.length === 0) {
-      /* define the least card range of given cards;
-      case when players don't have trumps*/
-      const playersWithSortCards = players.map(player => player.cards.sort((a, b) => {
-        if (a.range > b.range) {
+    const playersWithSortedCards = players.map((player) => {
+      player.cards.sort((a, b) => {
+        if (a.seniority > b.seniority) {
           return -1;
         }
-        if (a.range < b.range) {
+        if (a.seniority < b.seniority) {
           return 1;
         }
         return 0;
-      }));
-      return compareCards(players.slice());
-    }
-    return compareCards(playersWithTrumps);
+      });
+      return player;
+    });
+    const prepareCardsForCompare = ([pl1, pl2]) => {
+      if (pl1.cards.find(card => card.type === 'trump') || pl2.cards.find(card => card.type === 'trump')) {
+        const g = [pl1, pl2].map((player) => {
+          // const filteredCards = ;
+          // console.log(player.cards.filter(card => card.type === 'trump'));
+          player.cards = player.cards.filter(card => card.type === 'trump');
+          return player;
+        });
+        console.log(g);
+        return g;
+      }
+      return [pl1, pl2];
+    };
+    const preparedCards = prepareCardsForCompare(playersWithSortedCards);
+    const compareCards = ([pl1, pl2]) => {
+      const leastCardOfPlayer1 = pl1.cards.pop();
+      const leastCardOfPlayer2 = pl2.cards.pop();
+      if (!leastCardOfPlayer1 && !leastCardOfPlayer2) {
+        /* Here is required  the specific desicion */
+      }
+      if (!leastCardOfPlayer1) {
+        return players[1];
+      }
+      if (!leastCardOfPlayer2) {
+        return players[0];
+      }
+      if (leastCardOfPlayer1.seniority !== leastCardOfPlayer2.seniority) {
+        return leastCardOfPlayer1.seniority < leastCardOfPlayer2.seniority ?
+         players[0] : players[1];
+      }
+      return compareCards([pl1, pl2]);
+    };
+    return compareCards(preparedCards);
   };
   const runGame = () => {
     // definition who go first
