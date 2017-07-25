ce53eedefd32bb973213882bad2da517b4bcbae7
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-25 16:32:29 +0300

    add methods to Player.js

--- src/Player.js
+++ src/Player.js
@@ -18,10 +18,45 @@ export default class Player {
   addCards(arrOfCards) {
     return this.cardsOfGames.push(arrOfCards);
   }
-  take(card) {
+  takeCard(card) {
     return this.cards.push(card);
   }
-  give() {
-    return this.cards;// try to solve late
+  giveCards() {
+    const cardsOrdinary = this.cards.filter(card => card.type === 'ordinary');
+    const cardsTrump = this.cards.filter(card => card.type === 'trump');
+    if (cardsOrdinary.length === 0) {
+      cardsTrump.sort((a, b) => {
+        if (a.seniority > b.seniority) {
+          return -1;
+        }
+        if (a.seniority < b.seniority) {
+          return 1;
+        }
+        return 0;
+      });
+      const minCard1 = cardsTrump.pop();
+      const { name } = minCard1;
+      this.cards = this.cards.filter(card => card.name !== name);
+      return [minCard1];
+    }
+    cardsOrdinary.sort((a, b) => {
+      if (a.seniority > b.seniority) {
+        return -1;
+      }
+      if (a.seniority < b.seniority) {
+        return 1;
+      }
+      return 0;
+    });
+    const minCard2 = cardsOrdinary.pop();
+    const arrOfCards = cardsOrdinary.reduce((acc, card) => {
+      const { name } = acc[0];
+      if (card.name === name) {
+        return acc.push(card);
+      }
+      return acc;
+    }, [minCard2]).reverse();
+    this.cards = this.cards.filter(card => card.name !== minCard2.name);
+    return arrOfCards;
   }
 }
