4c8b87a6dec680b7dda4bb32d27d612e004f4d7b
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-25 13:54:17 +0300

    add methods to Player.js

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,21 @@ export default class Player {
     this.name = name;
     this.cards = cards;
   }
+  getName() {
+    return this.name;
+  }
+  getCards() {
+    return this.cards;
+  }
+  getCardsOfRounds() {
+    if (!this.cardsOfRounds) {
+      this.cardsOfRounds = [];
+    }
+    return this.cardsOfRounds;
+  }
+  addCards(arrOfCards) {
+    return this.cardsOfGames.push(arrOfCards);
+  }
   take(card) {
     return this.cards.push(card);
   }
