14bcd341b43fac651efc7a3cc9515b080488d64e
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-28 22:02:48 +0300

    add method takeCards

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,5 @@
+import sortFunction from './sortFunction';
+
 export default class Player {
   constructor(name, cards) {
     this.name = name;
@@ -9,45 +11,56 @@ export default class Player {
   getCards() {
     return this.cards;
   }
+  getCountOfCards() {
+    return this.cards.length;
+  }
   getCardsOfRounds() {
     if (!this.cardsOfRounds) {
       this.cardsOfRounds = [];
     }
     return this.cardsOfRounds;
   }
-  addCards(arrOfCards) {
-    return this.cardsOfGames.push(arrOfCards);
-  }
-  takeCard(card) {
+  addCardFromPack(card) {
     return this.cards.push(card);
+  }
+  takeCards(arrOfCards) {
+    const iter = ([head, ...rest], acc) => {
+      if (rest.length === 0) {
+        return acc;
+      }
+      const desiredCard = this.cards.find((card) => {
+        const checkingSeniority = card.seniority > head.seniority;
+        const checkingSuit = card.suit === head.suit;
+        if (card.type === head.type) {
+          return checkingSeniority && checkingSuit;
+        }
+        return card.type === 'trump' ? 1 : 0;
+      });
+      acc.push(desiredCard);
+      return iter(rest, acc);
+    };
+    const satisfactoryCards = iter(arrOfCards, []);
+    if (satisfactoryCards.length === arrOfCards.length) {
+      const setOfcards = new Set(satisfactoryCards);
+      this.status = 'go';
+      this.cards = this.cards.filter(cards => !setOfcards.has(cards));
+      return;
+    }
+    this.status = 'take';
+    this.cardsOfRounds.push(arrOfCards);
   }
   giveCards() {
     const cardsOrdinary = this.cards.filter(card => card.type === 'ordinary');
     const cardsTrump = this.cards.filter(card => card.type === 'trump');
+    this.status = 'go';
     if (cardsOrdinary.length === 0) {
-      cardsTrump.sort((a, b) => {
-        if (a.seniority > b.seniority) {
-          return -1;
-        }
-        if (a.seniority < b.seniority) {
-          return 1;
-        }
-        return 0;
-      });
+      cardsTrump.sort(sortFunction);
       const minCard1 = cardsTrump.pop();
       const { name } = minCard1;
       this.cards = this.cards.filter(card => card.name !== name);
       return [minCard1];
     }
-    cardsOrdinary.sort((a, b) => {
-      if (a.seniority > b.seniority) {
-        return -1;
-      }
-      if (a.seniority < b.seniority) {
-        return 1;
-      }
-      return 0;
-    });
+    cardsOrdinary.sort(sortFunction);
     const minCard2 = cardsOrdinary.pop();
     const arrOfCards = cardsOrdinary.reduce((acc, card) => {
       const { name } = acc[0];
