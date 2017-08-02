24ffa45407b600356e0fce8797736f8c3672cbb0
Dmitry Sedov <dmsedov92@gmail.com>
2017-08-02 18:21:20 +0300

    feature case when pack is empty (Player.js)

--- src/Player.js
+++ src/Player.js
@@ -1,4 +1,4 @@
-import {  sortByDecrease, sortByIncrease } from './sortFunctions';
+import { sortByDecrease, sortByIncrease } from './sortFunctions';
 
 export default class Player {
   constructor(name, cards) {
@@ -36,23 +36,27 @@ export default class Player {
   takeCards(arrOfCards) {
     const sortedCardsOfPlayer = this.cards.slice().sort(sortByIncrease);
     const iter = ([head, ...rest], cardsOfPlayer, acc) => {
-      const desiredCard = cardsOfPlayer.find((card, index, array) => {
+      const desiredCard1 = cardsOfPlayer.find((card, index, array) => {
         const checkingSeniority = card.seniority > head.seniority;
         const checkingSuit = card.suit === head.suit;
         const checkingType = card.type === head.type;
-        if (checkingType && checkingSeniority && checkingSuit) {
-          array.splice(index, 1);
-          return 1;
-        } else if (card.type === 'trump' && head.type === 'ordinary') {
+        if (checkingSeniority && checkingSuit && checkingType) {
           array.splice(index, 1);
           return 1;
         }
         return 0;
       });
-      // console.log(desiredCard, 'desired card!');
-      if (desiredCard) {
-        acc.push(desiredCard);
-        console.log(acc, 'acc!!!');
+      const desiredCard2 = cardsOfPlayer.find((card, index, array) => {
+        if (card.type === 'trump' && head.type === 'ordinary') {
+          array.splice(index, 1);
+          return 1;
+        }
+        return 0;
+      });
+      if (desiredCard1) {
+        acc.push(desiredCard1);
+      } else if (desiredCard2) {
+        acc.push(desiredCard2);
       }
       if (rest.length === 0) {
         return acc;
@@ -69,36 +73,81 @@ export default class Player {
     this.status = 'take';
     this.cards.push(...arrOfCards);
   }
-  giveCards() {
+  giveCards(pack) {
+    this.status = 'lead';
     const cardsOrdinary = this.cards.filter(card => card.getType() === 'ordinary');
     const cardsTrump = this.cards.filter(card => card.getType() === 'trump');
-    this.status = 'lead';
-    if (cardsOrdinary.length === 0) {
-      cardsTrump.sort(sortByDecrease);
-      const minCard1 = cardsTrump.pop();
-      const { name } = minCard1;
-      this.cards = this.cards.filter(card => card.name !== name);
-      return [minCard1];
-    }
-    cardsOrdinary.sort(sortByDecrease);
-    const minCard2 = cardsOrdinary.pop();
-    const arrOfCards = cardsOrdinary.reduce((acc, card) => {
-      const { name } = acc[0];
-      if (card.name === name) {
-        acc.push(card);
-        return acc;
+    const countTrumpCards = this.cards.reduce((acc, card) => {
+      if (card.type === 'trump') {
+        return acc + 1;
       }
       return acc;
-    }, [minCard2]).reverse();
-    this.cards = this.cards.filter((card) => {
-      if (card.name === minCard2.name && card.type !== minCard2.type) {
-        return 1;
-      } else if (card.name !== minCard2.name) {
-        return 1;
+    }, 0);
+    const countOrdinaryCards = this.cards.reduce((acc, card) => {
+      if (card.type === 'ordinary') {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    const defineWhichCardsGive = (trumpCards, ordinaryCards) => {
+      if (trumpCards.length === 0 && ordinaryCards.length === 0) {
+        return [];
+      }
+      if (ordinaryCards.length === 0) {
+        trumpCards.sort(sortByDecrease);
+        const leadCard1 = trumpCards.pop();
+        const { name } = leadCard1;
+        this.cards = this.cards.filter(card => card.name !== name);
+        return [leadCard1];
+      }
+      ordinaryCards.sort(sortByDecrease);
+      const leadCard2 = ordinaryCards.pop();
+      const arrOfCards = ordinaryCards.reduce((acc, card) => {
+        const { name } = acc[0];
+        if (card.name === name) {
+          acc.push(card);
+          return acc;
+        }
+        return acc;
+      }, [leadCard2]).reverse();
+      this.cards = this.cards.filter((card) => {
+        if (card.name === leadCard2.name && card.type !== leadCard2.type) {
+          return 1;
+        } else if (card.name !== leadCard2.name) {
+          return 1;
+        }
+        return 0;
+      });
+      return arrOfCards;
+    };
+    const hasSameSenTrumpCard = (trumpCards, ordinaryCards) => {
+      if (trumpCards.length !== 0 && ordinaryCards.length !== 0) {
+        const leastCard = ordinaryCards.slice().sort(sortByDecrease).pop();
+        const desiredCard = trumpCards.find(card => card.name === leastCard.name);
+        const assertion = desiredCard ? 1 : 0;
+        return assertion;
       }
       return 0;
-    });
-    console.log(arrOfCards, 'leading!');
-    return arrOfCards;
+    };
+    const delta = countTrumpCards - countOrdinaryCards;
+    if (pack.length === 0 && (delta >= countOrdinaryCards || delta === 0)
+       && hasSameSenTrumpCard(cardsTrump, cardsOrdinary)) {
+      const topCard = cardsOrdinary.sort(sortByDecrease).pop();
+      const cardsWithSameName = this.cards.slice().sort(sortByDecrease)
+      .filter(card => card.name === topCard.name);
+      const setOfSameCards = new Set(cardsWithSameName);
+      this.cards = this.cards.filter(cards => !setOfSameCards.has(cards));
+      const cardsOrdinary1 = this.cards.filter(card => card.getType() === 'ordinary');
+      const cardsTrump1 = this.cards.filter(card => card.getType() === 'trump');
+      const leadCards = defineWhichCardsGive(cardsTrump1, cardsOrdinary1);
+      this.cards.push(...cardsWithSameName);
+      if (leadCards.length === 0) {
+        const lastLeadCards = new Set(cardsWithSameName);
+        this.cards = this.cards.filter(cards => !lastLeadCards.has(cards));
+        return cardsWithSameName;
+      }
+      return leadCards;
+    }
+    return defineWhichCardsGive(cardsTrump, cardsOrdinary);
   }
 }
